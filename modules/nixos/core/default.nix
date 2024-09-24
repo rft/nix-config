c25d0345d35d4711250b76ebe4032dfd5e51dac8
rft <nano@nomolabs.net>
2024-09-23 19:05:49 -0700

    core module started

---
+++ modules/nixos/core/default.nix
@@ -0,0 +1,30 @@
+{ lib, pkgs, inputs, config, ...
+}:
+{
+  imports = [];
+
+  environment.systemPackages = with pkgs; [
+    picat
+    zellij 
+    ripgrep
+    distrobox
+    podman
+    podman-tui
+    fd
+    bat 
+    fzf
+    git 
+    neovim
+    jq
+    zoxide
+    xplr
+    bottom
+    pandoc
+    ffmpeg_7-full
+    procs
+    tldr
+    lazygit
+
+    xonsh
+  ];
+}
