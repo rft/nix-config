1bf1bf89b858e04c13d6b6aa9c14e1f35ab286c3
rft <nano@nomolabs.net>
2025-06-29 02:38:43 -0700

    added firacode to kitty

--- homes/x86_64-linux/nano/terminal/kitty/default.nix
+++ homes/x86_64-linux/nano/terminal/kitty/default.nix
@@ -5,10 +5,15 @@
   ...
 }:
 {
-    programs.kitty = {
-      enable = true;
-      settings = {
-        cursor_trail = 3;
-      };
+  programs.kitty = {
+    enable = true;
+    settings = {
+      cursor_trail = 3;
+      font_family = "FiraCode Nerd Font";
+
+      bold_font = "FiraCode Bold Nerd Font";
+      italic_font = "FiraCode Italic Nerd Font";
+      bold_italic_font = "FiraCode Bold Italic Nerd Font";
     };
+  };
 }
