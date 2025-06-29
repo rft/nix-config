c30b2d8956675a34be5926bf40d9f9e44f0af37f
rft <nano@nomolabs.net>
2025-06-29 03:29:26 -0700

    added extra packages to xonsh.

--- modules/nixos/core/xonsh.nix
+++ modules/nixos/core/xonsh.nix
@@ -12,5 +12,15 @@
     #nur.repos.xonsh-xontribs.xonsh
   ];
 
+  programs.xonsh = {
+    enable = true;
+    extraPackages =
+      ps: with ps; [
+        numpy
+        requests
+        xonsh.xontribs.xontrib-vox
+        #xonsh.xontribs.xontrib-prompt-starship
+      ];
+  };
 
 }
