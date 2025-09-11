991de512005d2cee2c5ea3cc339ae790d86b3b68
rft <nano@nomolabs.net>
2025-09-10 20:37:31 -0700

    removed sddm

--- systems/x86_64-linux/redwood/default.nix
+++ systems/x86_64-linux/redwood/default.nix
@@ -58,7 +58,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
