fdc0c081900610645f649ae1b038b09ee73972e0
rft <nano@nomolabs.net>
2025-10-30 19:31:58 -0700

    confirm on quit removed

--- homes/x86_64-linux/nano/terminal/kitty/default.nix
+++ homes/x86_64-linux/nano/terminal/kitty/default.nix
@@ -7,15 +7,17 @@
 {
   config = lib.mkIf config.modules.home.terminal.enable {
     programs.kitty = {
-    enable = true;
-    settings = {
-      cursor_trail = 3;
-      font_family = "FiraCode Nerd Font";
+      enable = true;
+      settings = {
+        cursor_trail = 3;
+        font_family = "FiraCode Nerd Font";
 
-      bold_font = "FiraCode Bold Nerd Font";
-      italic_font = "FiraCode Italic Nerd Font";
-      bold_italic_font = "FiraCode Bold Italic Nerd Font";
+        bold_font = "FiraCode Bold Nerd Font";
+        italic_font = "FiraCode Italic Nerd Font";
+        bold_italic_font = "FiraCode Bold Italic Nerd Font";
+        confirm_os_window_close = 0;
+        confirm_on_quit = 0;
+      };
     };
-  };
   };
 }
