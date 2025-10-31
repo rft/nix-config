04fb621e59bcf5374c291c44cccee41970dbbf2a
rft <nano@nomolabs.net>
2025-10-30 23:11:44 -0700

    set nord for noctalia

--- homes/x86_64-linux/nano/desktop/default.nix
+++ homes/x86_64-linux/nano/desktop/default.nix
@@ -19,6 +19,9 @@
   ];
 
   config = lib.mkIf config.modules.home.desktop.enable {
-    programs.noctalia-shell.enable = true;
+    programs.noctalia-shell = {
+      enable = true;
+      settings.colorSchemes.predefinedScheme = "Nord";
+    };
   };
 }
