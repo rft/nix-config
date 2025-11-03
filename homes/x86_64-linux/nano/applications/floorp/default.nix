cbab98297aa03ba9074612a62d51382461d88392
rft <nano@nomolabs.net>
2025-11-03 14:55:57 -0700

    extensions loaded

--- homes/x86_64-linux/nano/applications/floorp/default.nix
+++ homes/x86_64-linux/nano/applications/floorp/default.nix
@@ -13,6 +13,11 @@
           id = 0;
           isDefault = true;
           containersForce = true;
+          settings = {
+            # Keep Home Manager installed extensions enabled after launch.
+            "extensions.autoDisableScopes" = 0;
+            "extensions.enabledScopes" = 15;
+          };
           # Search here -> https://nur.nix-community.org/repos/rycee/
           extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
             auto-tab-discard
@@ -40,6 +45,30 @@
           ];
         };
       };
-    };
+      };
+
+    home.activation.floorpEnableExtensions =
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        profile_dir="${config.home.homeDirectory}/.floorp/main"
+        extensions_json="$profile_dir/extensions.json"
+        addon_startup="$profile_dir/addonStartup.json.lz4"
+        if [ -f "$extensions_json" ]; then
+          tmp_file="$extensions_json.hm-tmp"
+          ${pkgs.jq}/bin/jq '
+            if has("addons") then
+              .addons = (.addons
+                | map(if .foreignInstall == true then
+                    .userDisabled = false
+                    | .active = true
+                  else .
+                  end))
+            else .
+            end
+          ' "$extensions_json" > "$tmp_file" && mv "$tmp_file" "$extensions_json"
+        fi
+        if [ -f "$addon_startup" ]; then
+          rm -f "$addon_startup"
+        fi
+      '';
   };
 }
