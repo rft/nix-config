a123b37582893974dd94a7bc59ef56007f07f622
rft <nano@nomolabs.net>
2025-11-02 18:01:39 -0700

    updated vscode extensions

--- homes/x86_64-linux/nano/editors/vscode/default.nix
+++ homes/x86_64-linux/nano/editors/vscode/default.nix
@@ -30,55 +30,62 @@ in
   ];
   config = lib.mkIf config.modules.home.editors.enable {
     programs.vscode = {
-    package = vscodium-wayland;
-    enable = true;
-    mutableExtensionsDir = true;
-    profiles.default = {
-      enableExtensionUpdateCheck = true;
-      enableUpdateCheck = false;
-      extensions =
-      # https://search.nixos.org/packages?channel=25.05
-      (with pkgs.vscode-extensions; [
-        aaron-bond.better-comments
-        brettm12345.nixfmt-vscode
-        github.copilot
-        github.copilot-chat
-        jebbs.plantuml
-        jnoortheen.nix-ide
-        mechatroner.rainbow-csv
-        mkhl.direnv
-        ms-python.python
-        ms-python.debugpy
-        ms-python.vscode-pylance
-        ms-toolsai.jupyter
-        ms-vscode-remote.remote-ssh
-        ms-vscode.live-server
-        oderwat.indent-rainbow
-        usernamehw.errorlens
-        vscodevim.vim
-        vspacecode.vspacecode
-        vspacecode.whichkey
-        yzhang.markdown-all-in-one
-        #arrterian.nix-env-selector
-        svelte.svelte-vscode
-      ])
-      # Can be searched here -> https://marketplace.visualstudio.com/items?itemName=jacobdufault.fuzzy-search url shows the name
-      ++ (with marketplace; [
-        buenon.scratchpads
-        bodil.file-browser
-        jacobdufault.fuzzy-search
-        kahole.magit
-        maattdd.gitless
-        roipoussiere.cadquery
-        tonybaloney.vscode-pets
-        bernhard-42.ocp-cad-viewer
-        anthropic.claude-code
-        marimo-team.vscode-marimo
-        openai.chatgpt
-      ])
-      ++ (with marketplace-release; [
-      ]);
+      package = vscodium-wayland;
+      enable = true;
+      mutableExtensionsDir = true;
+      profiles.default = {
+        enableExtensionUpdateCheck = true;
+        enableUpdateCheck = false;
+        extensions =
+          # https://search.nixos.org/packages?channel=25.05
+          (with pkgs.vscode-extensions; [
+            aaron-bond.better-comments
+            brettm12345.nixfmt-vscode
+            github.copilot
+            github.copilot-chat
+            jebbs.plantuml
+            jnoortheen.nix-ide
+            mechatroner.rainbow-csv
+            mkhl.direnv
+            ms-python.python
+            ms-python.debugpy
+            ms-python.vscode-pylance
+            ms-toolsai.jupyter
+            ms-toolsai.jupyter-keymap
+            ms-toolsai.jupyter-renderers
+            ms-toolsai.vscode-jupyter-cell-tags
+            ms-toolsai.vscode-jupyter-slideshow
+            ms-vscode-remote.remote-ssh
+            ms-vscode.live-server
+            oderwat.indent-rainbow
+            usernamehw.errorlens
+            vscodevim.vim
+            vspacecode.vspacecode
+            vspacecode.whichkey
+            yzhang.markdown-all-in-one
+            #arrterian.nix-env-selector
+            svelte.svelte-vscode
+            streetsidesoftware.code-spell-checker
+            github.vscode-github-actions
+            charliermarsh.ruff
+          ])
+          # Can be searched here -> https://marketplace.visualstudio.com/items?itemName=jacobdufault.fuzzy-search url shows the name
+          ++ (with marketplace; [
+            buenon.scratchpads
+            bodil.file-browser
+            jacobdufault.fuzzy-search
+            kahole.magit
+            maattdd.gitless
+            roipoussiere.cadquery
+            tonybaloney.vscode-pets
+            bernhard-42.ocp-cad-viewer
+            anthropic.claude-code
+            marimo-team.vscode-marimo
+            openai.chatgpt
+          ])
+          ++ (with marketplace-release; [
+          ]);
+      };
     };
-  };
   };
 }
