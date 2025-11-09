4f972b8fd26333e6beccb9d18aab01dffc2b4c40
rft <nano@nomolabs.net>
2025-11-08 19:23:18 -0700

    changed extension and enablement

--- homes/x86_64-linux/nano/applications/floorp/default.nix
+++ homes/x86_64-linux/nano/applications/floorp/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   config = lib.mkIf config.modules.home.applications.enable {
     home.sessionVariables = {
@@ -21,7 +26,6 @@
           # Search here -> https://nur.nix-community.org/repos/rycee/
           extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
             auto-tab-discard
-            betterttv
             bitwarden
             clearurls
             darkreader
@@ -30,12 +34,10 @@
             gesturefy
             multi-account-containers
             old-reddit-redirect
-            privacy-badger
             reddit-enhancement-suite
             return-youtube-dislikes
             sidebery
             sponsorblock
-            stylus
             temporary-containers
             terms-of-service-didnt-read
             videospeed
@@ -45,30 +47,29 @@
           ];
         };
       };
-      };
+    };
 
-    home.activation.floorpEnableExtensions =
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        profile_dir="${config.home.homeDirectory}/.floorp/main"
-        extensions_json="$profile_dir/extensions.json"
-        addon_startup="$profile_dir/addonStartup.json.lz4"
-        if [ -f "$extensions_json" ]; then
-          tmp_file="$extensions_json.hm-tmp"
-          ${pkgs.jq}/bin/jq '
-            if has("addons") then
-              .addons = (.addons
-                | map(if .foreignInstall == true then
-                    .userDisabled = false
-                    | .active = true
-                  else .
-                  end))
-            else .
-            end
-          ' "$extensions_json" > "$tmp_file" && mv "$tmp_file" "$extensions_json"
-        fi
-        if [ -f "$addon_startup" ]; then
-          rm -f "$addon_startup"
-        fi
-      '';
+    home.activation.floorpEnableExtensions = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      profile_dir="${config.home.homeDirectory}/.floorp/main"
+      extensions_json="$profile_dir/extensions.json"
+      addon_startup="$profile_dir/addonStartup.json.lz4"
+      if [ -f "$extensions_json" ]; then
+        tmp_file="$extensions_json.hm-tmp"
+        ${pkgs.jq}/bin/jq '
+          if has("addons") then
+            .addons = (.addons
+              | map(if .foreignInstall == true then
+                  .userDisabled = false
+                  | .active = true
+                else .
+                end))
+          else .
+          end
+        ' "$extensions_json" > "$tmp_file" && mv "$tmp_file" "$extensions_json"
+      fi
+      if [ -f "$addon_startup" ]; then
+        rm -f "$addon_startup"
+      fi
+    '';
   };
 }
