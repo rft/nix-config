3fd1f5474f1498656d338e05a30457eb596e3c7b
rft <nano@nomolabs.net>
2026-02-21 02:19:36 -0700

    fixed kando service

--- homes/x86_64-linux/nano/applications/kando/default.nix
+++ homes/x86_64-linux/nano/applications/kando/default.nix
@@ -8,6 +8,23 @@
   config = lib.mkIf config.modules.home.applications.enable {
     home.packages = [ pkgs.kando ];
 
+    systemd.user.services.kando = {
+      Unit = {
+        Description = "Kando radial menu";
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${lib.getExe pkgs.kando} --background";
+        Restart = "on-failure";
+        RestartSec = 2;
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+
     xdg.configFile."kando/menus.json".text = ''
       {
         "version": "2.1.0",
