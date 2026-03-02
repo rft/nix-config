351da5870c3afdead4434f06439a3134963b4f67
rft <nano@nomolabs.net>
2026-03-02 04:31:19 +0000

    added teams to claude code

--- homes/x86_64-linux/nano/default.nix
+++ homes/x86_64-linux/nano/default.nix
@@ -77,16 +77,12 @@ in
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".claude/settings.json".text = builtins.toJSON {
+      env = {
+        CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS = "1";
+      };
+      teammateMode = "auto";
+    };
   };
 
   # Home Manager can also manage your environment variables through
