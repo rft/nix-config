b0fc1100d872d5735bef8010cb93f91a071f32e3
rft <nano@nomolabs.net>
2026-03-12 05:51:24 +0000

    kitty and starship fixes

--- modules/terminal/starship.nix
+++ modules/terminal/starship.nix
@@ -4,7 +4,73 @@ delib.module {
 
   options = delib.singleEnableOption true;
 
-  home.ifEnabled = {
+  home.ifEnabled =
+  let
+    # BMP Private Use Area glyphs get stripped by LLM tooling when stored
+    # as literal characters. Define them via JSON unicode escapes instead.
+    u = code: builtins.fromJSON ''"\\u${code}"'';
+
+    # Powerline arrows
+    arrow-right = u "e0b0";
+    arrow-left  = u "e0b2";
+
+    # Nerd Font icons (exact codepoints from original config)
+    ic-ssh      = u "eb01";
+    ic-terminal = u "f489";
+    ic-git      = u "f418";
+    ic-c        = u "e61e";
+    ic-elixir   = u "e275";
+    ic-elm      = u "e62c";
+    ic-go       = u "e627";
+    ic-python   = u "e73c";
+    ic-haskell  = u "e777";
+    ic-java     = u "e256";
+    ic-julia    = u "e624";
+    ic-nodejs   = u "e718";
+    ic-rust     = u "e7a8";
+    ic-scala    = u "e737";
+    ic-status   = u "f467";
+    ic-success  = u "eab2";
+    ic-signal   = u "e00a";
+
+    # OS distro icons
+    ic-alpaquita    = u "eaa2";
+    ic-alpine       = u "f300";
+    ic-amazon       = u "f270";
+    ic-android      = u "f17b";
+    ic-arch         = u "f303";
+    ic-artix        = u "f31f";
+    ic-centos       = u "f304";
+    ic-debian       = u "f306";
+    ic-dragonfly    = u "e28e";
+    ic-emscripten   = u "f205";
+    ic-endeavouros  = u "f197";
+    ic-fedora       = u "f30a";
+    ic-freebsd      = u "f30c";
+    ic-gentoo       = u "f30d";
+    ic-linux        = u "f31a";
+    ic-mabox        = u "eb29";
+    ic-macos        = u "f302";
+    ic-manjaro      = u "f312";
+    ic-mariner      = u "f1cd";
+    ic-midnightbsd  = u "f186";
+    ic-mint         = u "f30e";
+    ic-netbsd       = u "f024";
+    ic-nixos        = u "f313";
+    ic-opensuse     = u "f314";
+    ic-pop          = u "f32a";
+    ic-raspbian     = u "f315";
+    ic-redhat       = u "f316";
+    ic-ubuntu       = u "f31b";
+    ic-unknown      = u "f22d";
+
+    # Directory substitution icons
+    ic-download = u "f019";
+    ic-music    = u "f001";
+    ic-pictures = u "f03e";
+    ic-projects = u "ebdf";
+  in
+  {
     programs.starship = {
       enable = true;
       enableZshIntegration = true;
@@ -13,23 +79,23 @@ delib.module {
       settings = {
         format = lib.concatStrings [
           "[╭─](nord1)"
-          "[](nord0)"
+          "[${arrow-left}](nord0)"
           "[ 🌸 ](bg:nord0)"
           "$username"
           "[@](bg:nord0 fg:nord4)"
           "$hostname"
           "$os"
           "$shell"
-          "[](bg:nord1 fg:nord0)"
+          "[${arrow-right}](bg:nord1 fg:nord0)"
           "$directory"
-          "[](bg:nord2 fg:nord1)"
+          "[${arrow-right}](bg:nord2 fg:nord1)"
           "$git_branch"
           "$git_status"
           "$git_metrics"
-          "[](bg:nord3 fg:nord2)"
-          "[](fg:nord3)"
+          "[${arrow-right}](bg:nord3 fg:nord2)"
+          "[${arrow-right}](fg:nord3)"
           "$fill"
-          "[](nord0)"
+          "[${arrow-left}](nord0)"
           "[$status](bg:nord0 fg:nord4)"
           "$c"
           "$elixir"
@@ -44,11 +110,11 @@ delib.module {
           "$nim"
           "$rust"
           "$scala"
-          "[](bg:nord0 fg:nord2)"
+          "[${arrow-left}](bg:nord0 fg:nord2)"
           "$cmd_duration"
-          "[](bg:nord2 fg:nord1)"
+          "[${arrow-left}](bg:nord2 fg:nord1)"
           "$time"
-          "[](fg:nord1)"
+          "[${arrow-right}](fg:nord1)"
           "$line_break"
           "[╰─](nord1)"
           "$character"
@@ -79,7 +145,7 @@ delib.module {
 
         hostname = {
           ssh_only = false;
-          ssh_symbol = " ";
+          ssh_symbol = " ${ic-ssh}";
           trim_at = ".";
           format = "[$hostname$ssh_symbol]($style)";
           style = "bg:nord0 fg:nord4";
@@ -89,7 +155,7 @@ delib.module {
         shell = {
           disabled = false;
           style = "bg:nord0 fg:nord4";
-          format = "[  $indicator]($style)";
+          format = "[ ${ic-terminal} $indicator]($style)";
         };
 
         os = {
@@ -97,44 +163,44 @@ delib.module {
           style = "bg:nord0 fg:nord4";
           disabled = true;
           symbols = {
-            Alpaquita = " ";
-            Alpine = " ";
-            Amazon = " ";
-            Android = " ";
-            Arch = " ";
-            Artix = " ";
-            CentOS = " ";
-            Debian = " ";
-            DragonFly = " ";
-            Emscripten = " ";
-            EndeavourOS = " ";
-            Fedora = " ";
-            FreeBSD = " ";
+            Alpaquita = "${ic-alpaquita} ";
+            Alpine = "${ic-alpine} ";
+            Amazon = "${ic-amazon} ";
+            Android = "${ic-android} ";
+            Arch = "${ic-arch} ";
+            Artix = "${ic-artix} ";
+            CentOS = "${ic-centos} ";
+            Debian = "${ic-debian} ";
+            DragonFly = "${ic-dragonfly} ";
+            Emscripten = "${ic-emscripten} ";
+            EndeavourOS = "${ic-endeavouros} ";
+            Fedora = "${ic-fedora} ";
+            FreeBSD = "${ic-freebsd} ";
             Garuda = "󰛓 ";
-            Gentoo = " ";
+            Gentoo = "${ic-gentoo} ";
             HardenedBSD = "󰞌 ";
             Illumos = "󰈸 ";
-            Linux = " ";
-            Mabox = " ";
-            Macos = " ";
-            Manjaro = " ";
-            Mariner = " ";
-            MidnightBSD = " ";
-            Mint = " ";
-            NetBSD = " ";
-            NixOS = " ";
+            Linux = "${ic-linux} ";
+            Mabox = "${ic-mabox} ";
+            Macos = "${ic-macos} ";
+            Manjaro = "${ic-manjaro} ";
+            Mariner = "${ic-mariner} ";
+            MidnightBSD = "${ic-midnightbsd} ";
+            Mint = "${ic-mint} ";
+            NetBSD = "${ic-netbsd} ";
+            NixOS = "${ic-nixos} ";
             OpenBSD = "󰈺 ";
-            openSUSE = " ";
+            openSUSE = "${ic-opensuse} ";
             OracleLinux = "󰌷 ";
-            Pop = " ";
-            Raspbian = " ";
-            Redhat = " ";
-            RedHatEnterprise = " ";
+            Pop = "${ic-pop} ";
+            Raspbian = "${ic-raspbian} ";
+            Redhat = "${ic-redhat} ";
+            RedHatEnterprise = "${ic-redhat} ";
             Redox = "󰀘 ";
             Solus = "󰠳 ";
-            SUSE = " ";
-            Ubuntu = " ";
-            Unknown = " ";
+            SUSE = "${ic-opensuse} ";
+            Ubuntu = "${ic-ubuntu} ";
+            Unknown = "${ic-unknown} ";
             Windows = "󰍲 ";
           };
         };
@@ -150,10 +216,10 @@ delib.module {
           truncation_symbol = "";
           substitutions = {
             "Documents" = "󰈙";
-            "Downloads" = " ";
-            "Music" = " ";
-            "Pictures" = " ";
-            "Projects" = " ";
+            "Downloads" = "${ic-download} ";
+            "Music" = "${ic-music} ";
+            "Pictures" = "${ic-pictures} ";
+            "Projects" = "${ic-projects} ";
           };
         };
 
@@ -178,7 +244,7 @@ delib.module {
         };
 
         git_branch = {
-          symbol = "";
+          symbol = "${ic-git}";
           style = "bg:nord2 fg:nord4";
           format = "[ $symbol $branch ]($style)";
         };
@@ -196,31 +262,31 @@ delib.module {
         };
 
         c = {
-          symbol = " ";
+          symbol = "${ic-c} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         elixir = {
-          symbol = " ";
+          symbol = "${ic-elixir} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         elm = {
-          symbol = " ";
+          symbol = "${ic-elm} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         golang = {
-          symbol = " ";
+          symbol = "${ic-go} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         python = {
-          symbol = " ";
+          symbol = "${ic-python} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) (\($virtualenv\)) ]($style)";
         };
@@ -231,25 +297,25 @@ delib.module {
         };
 
         haskell = {
-          symbol = " ";
+          symbol = "${ic-haskell} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         java = {
-          symbol = " ";
+          symbol = "${ic-java} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         julia = {
-          symbol = " ";
+          symbol = "${ic-julia} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         nodejs = {
-          symbol = "";
+          symbol = "${ic-nodejs}";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
@@ -261,13 +327,13 @@ delib.module {
         };
 
         rust = {
-          symbol = "";
+          symbol = "${ic-rust}";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
 
         scala = {
-          symbol = " ";
+          symbol = "${ic-scala} ";
           style = "bg:nord0 fg:nord4";
           format = "[ $symbol ($version) ]($style)";
         };
@@ -275,12 +341,12 @@ delib.module {
         status = {
           format = "[ $symbol ]($style)";
           style = "bg:nord0 fg:nord4";
-          symbol = "[](fg:nord14 bg:nord0)";
-          success_symbol = "[](fg:nord14 bg:nord0)";
+          symbol = "[${ic-status}](fg:nord14 bg:nord0)";
+          success_symbol = "[${ic-success}](fg:nord14 bg:nord0)";
           not_executable_symbol = "[🛇](fg:nord11)";
           not_found_symbol = "[󰍉](fg:nord11)";
-          sigint_symbol = "[](fg:nord13)";
-          signal_symbol = "[](fg:nord11)";
+          sigint_symbol = "[${ic-signal}](fg:nord13)";
+          signal_symbol = "[${ic-signal}](fg:nord11)";
           disabled = false;
         };
       };
