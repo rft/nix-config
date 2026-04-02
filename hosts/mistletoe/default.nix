942a6d8a2d1696fb262b5524fa3e89b73720418f
rft <nano@nomolabs.net>
2026-04-02 21:16:44 +0000

    fixed conflict with wsl netbird and windows netbird

--- hosts/mistletoe/default.nix
+++ hosts/mistletoe/default.nix
@@ -15,6 +15,19 @@ delib.host {
 
     programs.nix-ld.enable = true;
     networking.hostName = "mistletoe";
+
+    # Fix MTU for WSL2 when Windows Netbird client sets adapter to 1280
+    systemd.services.fix-wsl-mtu = {
+      description = "Set eth0 MTU to 1500 for WSL2 Netbird compatibility";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "/run/current-system/sw/bin/ip link set eth0 mtu 1500";
+        RemainAfterExit = true;
+      };
+    };
   };
 
   myconfig = {
