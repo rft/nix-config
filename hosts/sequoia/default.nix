a5e3d07bef1225eea0a21d0597481c1ffd179c01
rft <nano@nomolabs.net>
2026-06-11 10:26:25 -0700

    fixed broken video playback on vmware

--- hosts/sequoia/default.nix
+++ hosts/sequoia/default.nix
@@ -48,6 +48,22 @@ delib.host {
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      # Running under VMware: the emulated HD-Audio driver returns bad timing info
+      # (snd_pcm_avail "Broken pipe"), so PipeWire's default timer-based scheduling
+      # underruns and stutters during video playback (146 xruns/15s observed).
+      # Switching the ALSA sink to IRQ/period-based scheduling fixes it (xruns -> 0).
+      wireplumber.extraConfig."99-vm-alsa-irq" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [ { "node.name" = "~alsa_output.*"; } ];
+            actions.update-props = {
+              "api.alsa.disable-tsched" = true;
+              "api.alsa.headroom" = 4096;
+              "api.alsa.period-size" = 1024;
+            };
+          }
+        ];
+      };
     };
 
     programs.firefox.enable = true;
