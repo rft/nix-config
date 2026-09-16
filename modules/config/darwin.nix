703eb086cd54b607e78309d8992e7557944d444c
rft <nano@nomolabs.net>
2026-09-15 17:13:42 -0700

    updated nehir and added chatgpt app

--- modules/config/darwin.nix
+++ modules/config/darwin.nix
@@ -33,7 +33,6 @@ delib.module {
         ];
         brews = [ "mas" ];
         casks = [
-          "nehir"
           "discord"
           "spotify"
           "obs"
@@ -56,6 +55,7 @@ delib.module {
           "espanso"
           "obsidian"
           "claude"
+          "chatgpt"
           "vorssaint"
         ];
         masApps = {
